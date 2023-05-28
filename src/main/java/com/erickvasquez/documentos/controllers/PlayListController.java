41cf9f50e50800227f863acf4c879e80b19c8791
ErickVasquez11 <00065520@uca.edu.sv>
2023-05-27 22:27:14 -0600

    :rocket: Add Controllers

---
+++ src/main/java/com/erickvasquez/documentos/controllers/PlayListController.java
@@ -0,0 +1,64 @@
+package com.erickvasquez.documentos.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.erickvasquez.documentos.models.dtos.playlists.SavePlayListDTO;
+import com.erickvasquez.documentos.models.dtos.songs.UpdateSongDTO;
+import com.erickvasquez.documentos.utils.RequestErrorHandler;
+
+@RestController
+@RequestMapping("/play-lists")
+@CrossOrigin("*")
+public class PlayListController {
+	
+	@Autowired
+	private RequestErrorHandler errorHandler;
+	
+	@GetMapping("")
+	public ResponseEntity<?> getPlayLists() {
+		return new ResponseEntity<>("playlists", HttpStatus.OK);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getPlayList() {
+		return new ResponseEntity<>("playlist", HttpStatus.OK);
+	}
+	
+	@PostMapping("")
+	public ResponseEntity<?> createPlayList(
+			@ModelAttribute SavePlayListDTO data, BindingResult validations) {
+		if (validations.hasErrors()) {
+			return new ResponseEntity<>(
+					errorHandler.mapErrors(validations.getFieldErrors()), HttpStatus.BAD_REQUEST);
+		}
+		
+		return new ResponseEntity<>(HttpStatus.CREATED);
+	}
+	
+	@PutMapping("")
+	public ResponseEntity<?> updatePlayList(
+			@ModelAttribute UpdateSongDTO data, BindingResult validations) {
+		if (validations.hasErrors()) {
+			return new ResponseEntity<>(
+					errorHandler.mapErrors(validations.getFieldErrors()), HttpStatus.BAD_REQUEST);
+		}
+		
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deletePlayList() {
+		return new ResponseEntity<>("deleted play list", HttpStatus.OK);
+	}
+}
